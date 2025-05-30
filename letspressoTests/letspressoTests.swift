4c5a732b11ec4ce9df4ebee83963ab63ac919873
Michael <mi.justus@icloud.com>
2025-05-30 19:18:25 +0200

    Initial Commit

---
+++ letspressoTests/letspressoTests.swift
@@ -0,0 +1,17 @@
+//
+//  letspressoTests.swift
+//  letspressoTests
+//
+//  Created by Michael on 30.05.25.
+//
+
+import Testing
+@testable import letspresso
+
+struct letspressoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
